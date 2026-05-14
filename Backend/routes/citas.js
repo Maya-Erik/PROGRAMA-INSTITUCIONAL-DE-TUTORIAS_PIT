be273156fdf3f5dbe96201c6fa67f48c38943105
ktrsca <ktrsca@gmail.com>
2026-05-13 18:04:43 -0600

    correciones

---
+++ Backend/routes/citas.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const controller = require('../controllers/citasController');
+const { verifyToken } = require('../middlewares/roleAuth');
+
+// Todas las rutas requieren autenticación
+router.use(verifyToken);
+
+router.get('/', controller.obtenerCitas);
+router.get('/mis-citas', controller.misCitas);
+router.post('/:id/inscribirse', controller.inscribirseCita);
+
+module.exports = router;
